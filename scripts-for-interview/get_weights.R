7dd1b646f2f380c626c14c3df2a7a21c56cc37a8
Jeremie Spagnolo <jeremie.spagnolo@gmail.com>
2023-08-03 11:19:49 -0700

    Update get_weights.R

--- scripts-for-interview/get_weights.R
+++ scripts-for-interview/get_weights.R
@@ -1,5 +1,9 @@
 
-## Create time weights
+## Load time weights. 
+## These were taken from an old project I did regarding user mock drafts.
+## User mock drafts were used to estimate prospect pick probabilities.
+## There, I looked at the effect of ranking date on ranking error.
+## LOESS was used to smooth out the effect and obtain weights.
 
 # Read weights data (based on days to draft) for Plackett-Luce model
 time_weights_for_pl <- read_csv('data/weights_for_pl.csv')
