f9fba677d4a8e1b7527160e2bcf819a5f9571b0f
Jeremie Spagnolo <jeremie.spagnolo@gmail.com>
2023-08-03 22:48:12 -0700

    add library sections

--- scripts-for-interview/libraries.R
+++ scripts-for-interview/libraries.R
@@ -1,10 +1,16 @@
 
-# Load necessary libraries
+## Load necessary libraries
+
+# libraries for data manipulation
 library(tidyverse)
 library(magrittr)
 library(janitor)
+
+# libraries for modeling
 library(PlackettLuce)
 library(PLMIX)
+
+# libraries for visualization
 library(wesanderson)
 library(ggridges)
 library(ggpubr)
